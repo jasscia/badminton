6ee4b7f0354c8e3e0b3184ac430fcfbbd1fc33b1
Jassica <1648511816@qq.com>
2018-03-08 23:20:26 +0800

    flex--grid,提示友好

--- pages/against/against.js
+++ pages/against/against.js
@@ -46,7 +46,7 @@ Page({
     if(!this.data.roundCount||this.data.personCount*this.data.roundCount%4){
       wx.showModal({ 
         title: "错误提示",
-        content: "场次和人数的乘积 不能被4整除",
+        content: "场次和人数的乘积 不能被4整除 排阵失败！！",
         mask:true,
         duration:2000
         })
@@ -66,11 +66,15 @@ Page({
           };
           this.setData({
             scoreList:res.data.AgainstTable,
-            showAgainstTable: true
+            // showAgainstTable: true,
           });
           wx.setStorage({
             key: 'scoreList',
             data: res.data.AgainstTable,
+          });
+          wx.showModal({
+            title: '友情建议',
+            content: `共为您排了${res.data.AgainstTable.length}场比赛，如果您的赛时为2小时，建议预定${Math.ceil(res.data.AgainstTable.length / 14)}个场地!\rps:每个场地不宜超过14场。`,
           })
         }
       })
