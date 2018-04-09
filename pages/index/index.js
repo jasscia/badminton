20867ef6fb4bcd41ac64ad7f64360cec1fc03aa7
Amypher <1648511816@qq.com>
2018-04-09 22:49:12 +0800

    简化排序 优化

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,103 +6,91 @@ const app = getApp()
 Page({
   data: {
     personList: [],
-    personListToString:null,
-    animationData:'',
+    personList_unordered: [],
+    personList_ordered: [],
+    personListToString: null,
+    animationData: '',
   },
   onLoad: function () {
-    var animation1 = wx.createAnimation({
-      duration: 1,
-      timingFunction: 'ease',
-    })
-    var animation2 = wx.createAnimation({
-      duration: 400,
-      timingFunction: 'ease',
-    })
-    this.animation1 = animation1; 
-    this.animation2 = animation2;
+    // this.setStorage('personList', []);
+    this.pos={startX:0,startY:0,endX:0,endY:0};
     this.initalPersonList();
+ 
   },
-  onShareAppMessage(){
+  onShareAppMessage() {
     return {
-      title:"CGGC羽球赛"
+      title: "CGGC羽球赛"
     }
-  }, 
+  },
   initalPersonList: function () {
     wx.getStorage({
       key: 'personList',
       success: res => {
         this.setData({
-            personList: res.data,
-            personListToString:res.data.join(' ')
-          })
+          personList: res.data,
+          personList_unordered: res.data,
+          personListToString: res.data.join(' ')
+        })
+
+        this.setStorage('personList', res.data);
+        this.setStorage('personList_ordered', res.data);
+        this.setStorage('personList_unordered', []);
       }
     })
   },
-  sortPersonList(e){
-    let dom = e.target,
-        domData=dom.dataset;
-    if(domData.type){
-      let order=domData.order;
-      let type=domData.type;
-      let originArr = this.data.personList
-      let currArr = this.changePosition(originArr,order-1,type)
-
-      this.dotAnimate(e,type);
-      this.setData({
-        personList: currArr,
-        personListToString:currArr.join(' ')
-      });
-    }
-  },
-  changePosition(array,index,way){
-    if(way==="up"){
-      let tem=array[index];
-      array[index]=array[index-1];
-      array[index-1]=tem;
-    }
-    if (way === "down") {
-      let tem = array[index];
-      array[index] = array[index + 1];
-      array[index + 1] = tem;
-    }
-    if (way === "remove") {
-      array.splice(index,1);
-    }
-    return array;
-  },
-  dotAnimate(event,way){
-    let index = event.target.dataset.order,
-        startYPos = (510+130*index-65)/2 -30 ,
-        endYPos = startYPos;
-    if (way === "up") { endYPos-=65}
-    if (way === "down") { endYPos+=65 }
-    console.log(index, startYPos, endYPos)//先很快的完成 将dot移动到当前位置
-    this.animation1.top(startYPos).opacity(1).step();
-    if (timerId) { clearTimeout(timerId)};
-     this.setData({
-      animationData: this.animation1.export()
-    })
-
-    //再缓慢的 将dot移动到下一个位置
-    let timerId=setTimeout(function () {
-      this.animation2.top(endYPos).opacity(0).scale(1).step();
-      this.setData({
-        animationData: this.animation2.export()
-      })
-    }.bind(this),50)
-  },
-  getPersonList(e){
-    let findNames= function(stringWithName){
+  getPersonList(e) {
+    let findNames = function (stringWithName) {
       return stringWithName.split(/[!！？？]+/).slice(-1)[0].split(/[\+\＋\s]+/);
     }
-    let content = e.detail.value;;
+    let content = e.detail.value;
     let personlist = findNames(content.trim());
     this.setData({
-      personList:personlist,
+      personList: personlist,
+      personList_unordered:personlist,
+      personList_ordered: []
     });
+
+    this.setStorage('personList', personlist);
+    this.setStorage('personList_ordered', personlist);
+    this.setStorage('personList_unordered', []);
   },
-  
-  submit:function () {
+
+unorderedMove(e) {
+  console.log(e.target.dataset.tag);
+  if (!e.target.dataset.tag){return};
+  let index = e.target.dataset.index;
+  let orderedList = this.data.personList_ordered;
+  let unorderedList = this.data.personList_unordered;
+  let list = orderedList.concat(unorderedList)
+  let value = null;
+  if (e.target.dataset.tag==='unordered') {
+    value = unorderedList[index]; 
+    unorderedList.splice(index, 1);
+    orderedList.push(value);
+   };
+   if (e.target.dataset.tag === 'ordered') {
+     value = orderedList[index];
+     orderedList.splice(index, 1);
+     unorderedList.push(value);
+   };
+  this.setData({
+    personList_unordered: unorderedList,
+    personList_ordered: orderedList,
+    personList: orderedList.concat(unorderedList),
+  })
+  this.setStorage('personList', list);
+  this.setStorage('personList_ordered', orderedList);
+  this.setStorage('personList_unordered', unorderedList);
+},
+touchEnd(e) {
+  this.pos.endX = e.detail.x,
+  this.pos.endY = e.detail.y;
+},
+touchStart(e){
+  this.pos.startX=e.detail.x,
+  this.pos.startY = e.detail.y;
+},
+  submit: function () {
     let realPersonList = this.data.personList.filter((name) => {
       return name
     });
@@ -113,15 +101,16 @@ Page({
       })
       return
     };
-    this.setStorage('personList',realPersonList);
+    this.setStorage('personList', realPersonList);
     wx.switchTab({
       url: '../against/against',
     })
   },
-  setStorage:function(key,data){
+  setStorage: function (key, data) {
     wx.setStorage({
       key,
       data
     })
   }
 })
+
